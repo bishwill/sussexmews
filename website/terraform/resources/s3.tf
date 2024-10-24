edfbd371aa2f7d5cd072c92268c9da421b6efa6e
bishwill <will.bishop9427@gmail.com>
2024-10-24 12:10:52 +0100

    make into a static website

--- website/terraform/resources/s3.tf
+++ website/terraform/resources/s3.tf
@@ -12,3 +12,16 @@ resource "aws_s3_bucket_policy" "allow_public_read_access" {
   bucket = aws_s3_bucket.this.id
   policy = data.aws_iam_policy_document.s3_public_access.json
 }
+
+
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
