a2f0b507b6c4ac3d494287343aef544869559d09
bishwill <will.bishop9427@gmail.com>
2025-02-23 13:09:36 +0000

    scripts file

--- src/sussexmews/__init__.py
+++ src/sussexmews/__init__.py
@@ -1,2 +1,17 @@
+import subprocess
+
+
 def main() -> None:
     print("Hello from sussexmews!")
+
+
+def format() -> None:
+    subprocess.run("uv run ruff format .", shell=True)
+
+
+def lint() -> None:
+    subprocess.run("uv run ruff check .", shell=True)
+
+
+def lintfix() -> None:
+    subprocess.run("uv run ruff check --fix .", shell=True)
