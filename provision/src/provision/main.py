0c0fb00949f801ff2a5391a4b2c1e6bd13438063
bishwill <will.bishop9427@gmail.com>
2025-02-23 13:07:07 +0000

    fix provision package

--- provision/src/provision/main.py
+++ provision/src/provision/main.py
@@ -6,8 +6,9 @@ from aws_cdk import Tags, Environment
 from provision.stacks import TraefikStack
 
 
-app = cdk.App()
-traefik_stack = TraefikStack(app, "traefik", env=Environment(account="499844099075", region="eu-west-2"))
-Tags.of(traefik_stack).add("stack", "traefik")
 
-app.synth()
+def main() -> None:
+    app = cdk.App()
+    traefik_stack = TraefikStack(app, "traefik", env=Environment(account="499844099075", region="eu-west-2"))
+    Tags.of(traefik_stack).add("stack", "traefik")
+    app.synth()
