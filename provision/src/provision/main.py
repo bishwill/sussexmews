24e67c4eb825a6dcebd37575c36841dd3ae003a6
Will Bishop <66740719+bishwill@users.noreply.github.com>
2025-02-23 13:16:13 +0000

    use aws cdk for iac (#17)
    * initial cdk project
    
    * migrate traefik stack to cdk
    
    * delete readme
    
    * remove ruff and re-add readme

---
+++ provision/src/provision/main.py
@@ -0,0 +1,13 @@
+import os
+
+import aws_cdk as cdk
+from aws_cdk import Tags, Environment
+
+from provision.stacks import TraefikStack
+
+
+app = cdk.App()
+traefik_stack = TraefikStack(app, "traefik", env=Environment(account="499844099075", region="eu-west-2"))
+Tags.of(traefik_stack).add("stack", "traefik")
+
+app.synth()
