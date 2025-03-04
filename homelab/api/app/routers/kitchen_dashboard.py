77925f9b4c86b12ad01ae6ac72bdafb965eb47a3
Will Bishop <will.bishop9427@gmail.com>
2025-03-04 20:07:17 +0000

    basic endpoint

--- homelab/api/app/routers/kitchen_dashboard.py
+++ homelab/api/app/routers/kitchen_dashboard.py
@@ -1,3 +1,6 @@
+import os
+
+import requests
 from fastapi import APIRouter
 
 
@@ -7,7 +10,22 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+AIRFLOW_SESSION = requests.session()
+AIRFLOW_SESSION.auth = (os.environ["AIRFLOW_USER"], os.environ["AIRFLOW_PWD"])
+AIRFLOW_SESSION.headers = {"accept": "application/json"}
+
 
 @router.get("/")
 async def hello():
     return {"message": "Hello from Kitchen Dashboard!"}
+
+
+@router.post("/update")
+async def update_dashboard():
+    response = AIRFLOW_SESSION.post(
+        "https://airflow.sussexmews.co.uk/api/v1/dags/bills_history_tracker/dagRuns",
+        json={}
+    )
+    response.raise_for_status()
+
+    return {"message": f"Dashboard Updated!"}
