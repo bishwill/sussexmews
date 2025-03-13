a23128f7300c739eff7fb55d82c3d8605c761922
wbishop-ea <will.bishop@energyaspects.com>
2025-03-13 14:59:41 +0000

    patches

--- homelab/api/app/routers/kitchen_dashboard.py
+++ homelab/api/app/routers/kitchen_dashboard.py
@@ -1,12 +1,8 @@
-import os
 
-import requests
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
 from app.lib.ssh import (
-    CatfordCastleMiniSSHClient,
-    CatfordCastleSSHClient,
     SussexMewsSSHClient,
 )
 from app.models.kitchen_dashboard import KitchenDashboardUpdateRequest
@@ -35,4 +31,4 @@ async def update_dashboard(update_request: KitchenDashboardUpdateRequest):
     # reboot
     ssh_client.execute("sudo shutdown -r")
 
-    return {"msg": "URL successfully updated. Dashboard will reboot in 1 minute"}
+    return JSONResponse(content={"msg": "URL successfully updated. Dashboard will reboot in 1 minute"})
