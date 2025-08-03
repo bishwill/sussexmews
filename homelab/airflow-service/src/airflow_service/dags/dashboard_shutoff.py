0041b14c2378010a260fdc1d187093ce4a04c332
Will Bishop <will.bishop9427@gmail.com>
2025-08-03 20:24:00 +0000

    shutoff DAG

---
+++ homelab/airflow-service/src/airflow_service/dags/dashboard_shutoff.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.models import Variable
+
+
+BASE_URL = Variable.get("SUSSEXMEWS_API_BASE_URL")
+HEADERS = "-H 'accept: application/json' -H 'Content-Type: application/json'"
+DATA = """-d '{"screen": "right"}'"""
+
+
+dag = DAG(
+    "dashboard_shutoff",
+    default_args={
+        "owner": "will",
+        "retries": 0,
+    },
+    description="Shuts down the Raspberry Pis running the kitchen dashboard.",
+    schedule_interval="0 22 * * *",
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+)
+
+for screen_side in ("left", "right"):
+    task = BashOperator(
+        task_id=f"shutoff_{screen_side}_screen",
+        bash_command=f"curl --fail -X 'POST' '{BASE_URL}/kitchen-dashboard/shutdown' {HEADERS} {DATA}",
+        dag=dag,
+    )
