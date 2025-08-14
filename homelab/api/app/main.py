617ba6e138260d1b5aae83934f939b6a1c27f3be
bishwill <will.bishop9427@gmail.com>
2025-08-14 18:53:19 +0100

    Squashed commit of the following:
    commit f0b2f7a96fee2eeeaf9ce7738271c9c98defeee0
    Author: bishwill <will.bishop9427@gmail.com>
    Date:   Wed Aug 6 09:47:49 2025 +0100
    
        fmt
    
    commit 42a1d7484017f9f609b1bc909251cc97d5733059
    Author: Will Bishop <will.bishop9427@gmail.com>
    Date:   Wed Aug 6 08:44:57 2025 +0000
    
        spelling
    
    commit 7281d9397f6606d652cfa07635ef8c237606a81a
    Author: Will Bishop <will.bishop9427@gmail.com>
    Date:   Wed Aug 6 08:42:53 2025 +0000
    
        correct command for left screen
    
    commit 54c6838a55124dc6684254c23e1ac58053473882
    Author: Will Bishop <will.bishop9427@gmail.com>
    Date:   Tue Aug 5 20:23:46 2025 +0000
    
        format
    
    commit ca048e1c2e4c6e6a0e958f2a0180dde7e482c88e
    Author: Will Bishop <will.bishop9427@gmail.com>
    Date:   Tue Aug 5 20:23:24 2025 +0000
    
        reboot endpoint
    
    commit d0fd4cfb77db8aeba6425688efeb8b7b314f0df6
    Author: Will Bishop <will.bishop9427@gmail.com>
    Date:   Sun Aug 3 20:51:23 2025 +0000
    
        bump versions
    
    commit 6da04582aa6765f607ae910750d7b66ea22d2e14
    Author: Will Bishop <will.bishop9427@gmail.com>
    Date:   Sun Aug 3 20:50:56 2025 +0000
    
        bump version
    
    commit db755931409837d3ebe7362306dfe104300fc1a1
    Author: Will Bishop <will.bishop9427@gmail.com>
    Date:   Sun Aug 3 20:48:22 2025 +0000
    
        correct timezone
    
    commit 0041b14c2378010a260fdc1d187093ce4a04c332
    Author: Will Bishop <will.bishop9427@gmail.com>
    Date:   Sun Aug 3 20:24:00 2025 +0000
    
        shutoff DAG
    
    commit 0c9ddfb4087185e116c337873ac2b6d2be830f44
    Author: Will Bishop <will.bishop9427@gmail.com>
    Date:   Sun Aug 3 20:09:35 2025 +0000
    
        add shutdown endpoint

--- homelab/api/app/main.py
+++ homelab/api/app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from .routers import kitchen_dashboard
 
 
-app = FastAPI(title="Sussex Mews Rest API", version="0.1.1")
+app = FastAPI(title="Sussex Mews Rest API", version="0.2.0")
 
 
 app.include_router(kitchen_dashboard.router)
--- homelab/api/app/routers/kitchen_dashboard.py
+++ homelab/api/app/routers/kitchen_dashboard.py
@@ -4,7 +4,11 @@ from fastapi.responses import JSONResponse
 from app.lib.ssh import (
     SussexMewsSSHClient,
 )
-from app.models.kitchen_dashboard import KitchenDashboardUpdateRequest
+from app.models.kitchen_dashboard import (
+    KitchenDashboardUpdateRequest,
+    KitchenDashboardShutdownRequest,
+    KitchenDashboardRebootRequest,
+)
 
 
 router = APIRouter(
@@ -23,8 +27,28 @@ async def update_dashboard(update_request: KitchenDashboardUpdateRequest):
     ssh_client.execute(update_url_command).splitlines()[0]
 
     # reboot
-    ssh_client.execute("sudo shutdown -r")
+    ssh_client.execute("sudo reboot -h now")
+
+    return JSONResponse(content={"msg": "URL successfully updated!"})
+
+
+@router.post("/shutdown")
+async def shutdown_dashboard(shutdown_request: KitchenDashboardShutdownRequest):
+    shutdown_request.ssh_client().execute("sudo shutdown -h now")
 
     return JSONResponse(
-        content={"msg": "URL successfully updated. Dashboard will reboot in 1 minute"}
+        content={
+            "msg": f"{shutdown_request.screen.capitalize()} screen successfullly shutdown!"
+        }
+    )
+
+
+@router.post("/reboot")
+async def reboot_dashboard(reboot_request: KitchenDashboardRebootRequest):
+    reboot_request.ssh_client().execute("sudo reboot -h now")
+
+    return JSONResponse(
+        content={
+            "msg": f"{reboot_request.screen.capitalize()} screen successfullly rebooted!"
+        }
     )
